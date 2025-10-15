ce0ae35c458d8453d0ef9d14bfa3f3d73c372346
KAMALCODES <kamalcodes.pro@gmail.com>
2025-10-15 17:24:06 -0400

    first commit

---
+++ src/test/java/com/apifactory/client_contract_api/ClientContractApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apifactory.client_contract_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContractApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
